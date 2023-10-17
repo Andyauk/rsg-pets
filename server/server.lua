0662fcc09ae5573933b4c33d981102a58c742b3f
Alexis Percepied <32911879+Percxis@users.noreply.github.com>
2023-10-17 20:03:09 +0200

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,67 +1,16 @@
 local RSGCore = exports['rsg-core']:GetCoreObject()
 
--- foxhound
-RSGCore.Functions.CreateUseableItem("foxhound", function(source, item)
-    local Player = RSGCore.Functions.GetPlayer(source)
-    TriggerClientEvent("rsg-pets:client:callfoxhound", source, item.name)
-end)
+CreateUseableItem = function()
+    -- Obtention de la liste des animaux référencés dans le fichier de configuration.
+    for k, v in pairs(Config.PetShop) do
 
--- sheperd
-RSGCore.Functions.CreateUseableItem("sheperd", function(source, item)
-    local Player = RSGCore.Functions.GetPlayer(source)
-    TriggerClientEvent("rsg-pets:client:callsheperd", source, item.name)
-end)
+        -- Création de l'item (action)
+        RSGCore.Functions.CreateUseableItem(v.name, function(source)
+            TriggerClientEvent("rsg-pets:client:call", source, v.name, k)
+        end)
 
--- coonhound
-RSGCore.Functions.CreateUseableItem("coonhound", function(source, item)
-    local Player = RSGCore.Functions.GetPlayer(source)
-    TriggerClientEvent("rsg-pets:client:callcoonhound", source, item.name)
-end)
+    end
+end
 
--- catahoulacur
-RSGCore.Functions.CreateUseableItem("catahoulacur", function(source, item)
-    local Player = RSGCore.Functions.GetPlayer(source)
-    TriggerClientEvent("rsg-pets:client:callcatahoulacur", source, item.name)
-end)
-
--- bayretriever
-RSGCore.Functions.CreateUseableItem("bayretriever", function(source, item)
-    local Player = RSGCore.Functions.GetPlayer(source)
-    TriggerClientEvent("rsg-pets:client:callbayretriever", source, item.name)
-end)
-
--- collie
-RSGCore.Functions.CreateUseableItem("collie", function(source, item)
-    local Player = RSGCore.Functions.GetPlayer(source)
-    TriggerClientEvent("rsg-pets:client:callcollie", source, item.name)
-end)
-
--- hound
-RSGCore.Functions.CreateUseableItem("hound", function(source, item)
-    local Player = RSGCore.Functions.GetPlayer(source)
-    TriggerClientEvent("rsg-pets:client:callhound", source, item.name)
-end)
-
--- husky
-RSGCore.Functions.CreateUseableItem("husky", function(source, item)
-    local Player = RSGCore.Functions.GetPlayer(source)
-    TriggerClientEvent("rsg-pets:client:callhusky", source, item.name)
-end)
-
--- lab
-RSGCore.Functions.CreateUseableItem("lab", function(source, item)
-    local Player = RSGCore.Functions.GetPlayer(source)
-    TriggerClientEvent("rsg-pets:client:calllab", source, item.name)
-end)
-
--- poodle
-RSGCore.Functions.CreateUseableItem("poodle", function(source, item)
-    local Player = RSGCore.Functions.GetPlayer(source)
-    TriggerClientEvent("rsg-pets:client:callpoodle", source, item.name)
-end)
-
--- street
-RSGCore.Functions.CreateUseableItem("street", function(source, item)
-    local Player = RSGCore.Functions.GetPlayer(source)
-    TriggerClientEvent("rsg-pets:client:callstreet", source, item.name)
-end)
+--| Création des items au lancement du script.
+CreateUseableItem()
