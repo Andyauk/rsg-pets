a2dd6c490657c5b4bd91e74899d07843d12dfb19
Andyauk <47583709+Andyauk@users.noreply.github.com>
2023-12-20 13:22:43 +0000

    oxlib support - update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,10 +4,11 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 lua54 'yes'
 
 description 'rsg-pets'
-version '1.0.0'
+version '1.0.1'
 
 shared_scripts {
     'shared/*.lua',
+    '@ox_lib/init.lua'
 }
 
 client_scripts {
@@ -20,4 +21,5 @@ server_scripts {
 
 dependencies {
     'rsg-core',
+    'ox_lib'
 }
